12ce77ac26dccafaf990b9596f6b3aa70b344f2d
Adil Rouibet <46888737+S00189549@users.noreply.github.com>
2020-05-13 11:41:20 +0100

    Database created , not filled

---
+++ adilRouibet_s00189549/DataBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adilRouibet_s00189549
+{
+    public class PhonesDataBase : DbContext
+    {
+        public PhonesDataBase() : base("PhonesDataBase") { }
+
+        public DbSet<Phone> phones { get; set; }
+
+    }
+}
